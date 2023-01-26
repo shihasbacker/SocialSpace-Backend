e3c65cdd4fb185ce3532cbd31df02a01e24ad9f6
shihasbacker <shihasbacker@yahoo.com>
2023-01-26 12:02:13 +0530

    multer filter

--- Routes/UploadRoute.js
+++ Routes/UploadRoute.js
@@ -4,13 +4,36 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "Public/Images");  
+    cb(null, "Public/Images");
   },
   filename: (req, file, cb) => {
     cb(null, req.body.name);
   },
 });
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, callback) {
+    var ext = path.extname(file.originalname);
+    if (ext !== ".png" && ext !== ".jpg" && ext !== ".gif" && ext !== ".jpeg") {
+      return callback("Only images are allowed");
+    }
+    callback(null, true);
+  },
+});
+
+// var upload = multer({ //multer settings
+//   storage: storage,
+//   fileFilter: function (req, file, callback) {
+//       var ext = path.extname(file.originalname);
+//       if(ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
+//           return callback(new Error('Only images are allowed'))
+//       }
+//       callback(null, true)
+//   },
+//   limits:{
+//       fileSize: 1024 * 1024
+//   }
+// }).single('profilepic');
 
 router.post(
   "/",
